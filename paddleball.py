20bd6448161160981b8675b27fda0a6b48e453f2
ZhipingJ <2581998321@qq.com>
2016-12-04 19:42:38 +0800

    检测是否撞到球拍

--- paddleball.py
+++ paddleball.py
@@ -4,8 +4,9 @@ import time
 
 
 class Ball:
-        def __init__(self,canvas,color):
+        def __init__(self,canvas,paddle,color):
                 self.canvas=canvas
+                self.paddle=paddle
                 self.id=self.canvas.create_oval(10,10,25,25,fill=color)
                 self.canvas.move(self.id,245,100)
                 starts=[-3,-2,-1,1,2,3]
@@ -14,6 +15,14 @@ class Ball:
                 self.y=-3
                 self.canvas_height=self.canvas.winfo_height()
                 self.canvas_width=self.canvas.winfo_width()
+                self.hit_bottom=False
+
+        def hit_paddle(self,pos):
+                paddle_pos=self.canvas.coords(self.paddle.id)
+                if pos[2]>=paddle_pos[0] and pos[0]<=paddle_pos[2]:
+                        if pos[3]>=paddle_pos[1] and pos[3]<=paddle_pos[3]:
+                                return True
+                return False
 
         def draw(self):
                 self.canvas.move(self.id,self.x,self.y)
@@ -21,6 +30,8 @@ class Ball:
                 if pos[1]<=0:
                         self.y=6
                 if pos[3]>=self.canvas_height:
+                        self.hit_bottom=True
+                if self.hit_paddle(pos)==True:
                         self.y=-6
                 if pos[0]<=0:
                         self.x=6
@@ -29,14 +40,27 @@ class Ball:
 
 
 class Paddle:
-	"""docstring for Paddle"""
-	def __init__(self, canvas,color):
-		self.canvas=canvas
-		self.id=canvas.create_rectangle(0,0,100,10,fill=color)
-		self.canvas.move(self.id,200,300)
-	def draw(self):
-		pass
-		
+        def __init__(self, canvas,color):
+                self.canvas=canvas
+                self.id=canvas.create_rectangle(0,0,100,10,fill=color)
+                self.canvas.move(self.id,200,300)
+                self.x=0
+                self.canvas_width=self.canvas.winfo_width()
+                self.canvas.bind_all('<KeyPress-Left>',self.turn_left)
+                self.canvas.bind_all('<KeyPress-Right>',self.turn_right)
+
+        def draw(self):
+                self.canvas.move(self.id,self.x,0)
+                pos=self.canvas.coords(self.id)
+                if pos[0]<=0:
+                        self.x=0
+                elif pos[2]>=self.canvas_width:
+                        self.x=0
+        def turn_left(self,evt):
+                self.x=-2
+        def turn_right(self,evt):
+                self.y=2
+
 
 
 tk=Tk()
@@ -49,11 +73,14 @@ tk.update()
 
 
 paddle=Paddle(canvas,'blue')
-ball=Ball(canvas,'red')
+ball=Ball(canvas,paddle,'red')
 
-while 1:		
+while 1:
+        if ball.hit_bottom==False:
+                ball.draw()
+                paddle.draw()
         ball.draw()
         paddle.draw()
         tk.update_idletasks()
         tk.update()
-        time.sleep(0.01)
+        time.sleep(0.05)
